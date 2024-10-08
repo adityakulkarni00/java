af50b32149d7e6c401ca71ce2e94bf5f4e1e810d
adityakulkarni00 <adityakulkarni00@gmail.com>
2024-10-08 16:48:00 +0530

    2

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -2,6 +2,6 @@ package com.mycompany.app;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World test 1!");
+        System.out.println("Hello World test 2!");
     }
 }
